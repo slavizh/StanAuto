3da7d44c1b5798e27b5e58dd2d635430110fa2dd
Stanislav Zhelyazkov <stas@outlook.com>
2017-02-13 09:19:57 +0200

    Add files via upload

---
+++ SetTimeZone.ps1
@@ -0,0 +1,14 @@
+﻿Configuration SetTimeZone
+{
+
+   Import-DSCResource -ModuleName xTimeZone
+
+   Node TimeZone
+   {
+        xTimeZone TimeZoneExample
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = 'E. Europe Standard Time'
+        }
+   }
+}
